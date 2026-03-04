3dbb5e30aa1fbdb9ddaa6db4e8c49982371f7f6e
Giovanna Praieiro Pavani <gigipraieiro@gmail.com>
2026-03-04 10:41:02 -0300

    feat: atualiza métodos e diagrama UML

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,42 +1,28 @@
 package br.com.fiapride.main;
-
-import br.com.fiapride.model.Paletadesombra;
-
+import br.com.fiapride.model.PaletaDeSombra;
 public class SistemaPrincipal {
-
-    public static void main(String[] args) {
-
-        // Primeira paleta
-        Paletadesombra minhaPaleta = new Paletadesombra();
-        minhaPaleta.acabamento = "Matte";
-        minhaPaleta.nivelPigmentacao = 8;
-        minhaPaleta.horasDuracao = 12;
-        minhaPaleta.quantidadeCores = 16;
-        minhaPaleta.estaQuebrada = false;
-
-        // Segunda paleta
-        Paletadesombra paletaDaAmiga = new Paletadesombra();
-        paletaDaAmiga.acabamento = "Cintilante";
-        paletaDaAmiga.nivelPigmentacao = 6;
-        paletaDaAmiga.horasDuracao = 8;
-        paletaDaAmiga.quantidadeCores = 9;
-        paletaDaAmiga.estaQuebrada = true;
-
-        System.out.println("--- Sistema de Paletas ---");
-
-        System.out.println("Minha paleta: " 
-                + "\nAcabamento: " + minhaPaleta.acabamento
-                + "\nPigmentação: " + minhaPaleta.nivelPigmentacao
-                + "\nDuração (horas): " + minhaPaleta.horasDuracao
-                + "\nQuantidade de cores: " + minhaPaleta.quantidadeCores
-                + "\nEstá quebrada? " + minhaPaleta.estaQuebrada
-                + "\n");
-
-        System.out.println("Paleta da amiga: " 
-                + "\nAcabamento: " + paletaDaAmiga.acabamento
-                + "\nPigmentação: " + paletaDaAmiga.nivelPigmentacao
-                + "\nDuração (horas): " + paletaDaAmiga.horasDuracao
-                + "\nQuantidade de cores: " + paletaDaAmiga.quantidadeCores
-                + "\nEstá quebrada? " + paletaDaAmiga.estaQuebrada);
-    }
+   public static void main(String[] args) {
+       // Paleta funcionando
+       PaletaDeSombra minhaPaleta =
+               new PaletaDeSombra("Matte", 8, 12, 16);
+       // Paleta da amiga
+       PaletaDeSombra paletaDaAmiga =
+               new PaletaDeSombra("Cintilante", 6, 8, 9);
+       System.out.println("=== TESTE DO SISTEMA ===");
+       // Testando paleta funcionando
+       System.out.println("\nMinha paleta:");
+       minhaPaleta.aplicarSombra(3);
+       minhaPaleta.esfumarSombra(2);
+       // Quebrando a segunda paleta
+       System.out.println("\nPaleta da amiga:");
+       paletaDaAmiga.quebrarPaleta();
+       // Tentando usar quebrada
+       paletaDaAmiga.aplicarSombra(2);
+       // Reparando e usando novamente
+       paletaDaAmiga.repararPaleta();
+       paletaDaAmiga.aplicarSombra(2);
+       // Teste inválido
+       System.out.println("\nTeste inválido:");
+       minhaPaleta.aplicarSombra(-1);
+   }
 }
--- src/br/com/fiapride/model/Paletadesombra.java
+++ src/br/com/fiapride/model/Paletadesombra.java
@@ -1,9 +1,66 @@
 package br.com.fiapride.model;
-
-public class Paletadesombra {
-	public String acabamento;
-    public int nivelPigmentacao;
-    public int horasDuracao;
-    public int quantidadeCores;
-    public boolean estaQuebrada;
+public class PaletaDeSombra {
+   // Atributos
+   private String acabamento;
+   private int nivelPigmentacao;
+   private int horasDuracao;
+   private int quantidadeCores;
+   private boolean estaQuebrada;
+   // Construtor
+   public PaletaDeSombra(String acabamento, int nivelPigmentacao,
+                         int horasDuracao, int quantidadeCores) {
+       this.acabamento = acabamento;
+       this.nivelPigmentacao = nivelPigmentacao;
+       this.horasDuracao = horasDuracao;
+       this.quantidadeCores = quantidadeCores;
+       this.estaQuebrada = false;
+   }
+   // Aplicar sombra
+   public void aplicarSombra(int intensidade) {
+       if (intensidade <= 0) {
+           System.out.println("Erro: intensidade inválida.");
+           return;
+       }
+       if (estaQuebrada) {
+           System.out.println("Erro: a paleta está quebrada.");
+           return;
+       }
+       if (horasDuracao <= 0) {
+           System.out.println("Erro: a paleta não possui mais durabilidade.");
+           return;
+       }
+       nivelPigmentacao += intensidade;
+       horasDuracao -= 1;
+       System.out.println("Sombra aplicada com intensidade " + intensidade);
+   }
+   // Esfumar sombra
+   public void esfumarSombra(int nivelSuavizacao) {
+       if (nivelSuavizacao <= 0) {
+           System.out.println("Erro: nível inválido para esfumar.");
+           return;
+       }
+       if (nivelPigmentacao <= 0) {
+           System.out.println("Erro: não há pigmentação para esfumar.");
+           return;
+       }
+       nivelPigmentacao -= nivelSuavizacao;
+       if (nivelPigmentacao < 0) {
+           nivelPigmentacao = 0;
+       }
+       System.out.println("Sombra esfumada com sucesso.");
+   }
+   // Quebrar paleta
+   public void quebrarPaleta() {
+       estaQuebrada = true;
+       System.out.println("A paleta foi quebrada!");
+   }
+   // Reparar paleta
+   public void repararPaleta() {
+       if (!estaQuebrada) {
+           System.out.println("A paleta já está em perfeito estado.");
+           return;
+       }
+       estaQuebrada = false;
+       System.out.println("A paleta foi reparada com sucesso!");
+   }
 }
